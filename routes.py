81076db1a7ef8d856b7c0b2386d291fa364d5dc7
Eric Borenstein <e.s.borenstein@gmail.com>
2019-10-22 16:13:53 -0400

    Added reform command call

--- routes.py
+++ routes.py
@@ -1,23 +1,27 @@
 import os
 import wget
+import reform
 from flask import Flask, render_template, request, flash, redirect, url_for
 from werkzeug import secure_filename
 app = Flask(__name__)
 
 UPLOAD_FOLDER = './uploads'
-ALLOWED_EXTENSIONS = set(['fa', 'gff', 'gff3', 'gtf'])
+ALLOWED_EXTENSIONS = set(['fa', 'gff', 'gff3', 'gtf', 'fasta', 'fna', 'tar.gz', 'tar', 'gz'])
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = 'super secret key'
 
 @app.route('/')
-@app.route('/upload')
+@app.route('/submit')
 def upload_file():
    return render_template('submit.html')
 	
-@app.route('/result', methods = ['GET', 'POST'])
-def uploader():
+@app.route('/submitted', methods = ['GET', 'POST'])
+def grab():
    if request.method == 'POST':
+      
+      os.system('rm ./uploads/*')
+      
       # Upload Files
       upstream_fasta = request.files['upstream_fasta']
       upstream_fasta.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(upstream_fasta.filename)))
@@ -38,24 +42,17 @@ def uploader():
       ref_gff_url = request.form['ref_gff']
       ref_gff_download = wget.download(ref_gff_url, UPLOAD_FOLDER)
 
-#      os.system('python3 /home/eric/apps/reformWeb/reform.py --chrom {} --position {} --in_fasta {} --in_gff {} --ref_fasta {} --ref_gff {} > out'.format(
-#           request.form['chrom'],
-#           request.form['position'],
-#           os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(in_fasta.filename)),
-#           os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(in_gff.filename)),
-#           os.path.join(app.config['UPLOAD_FOLDER'], ref_fasta_download ),
-#           os.path.join(app.config['UPLOAD_FOLDER'], ref_gff_download ) ))
-#      flash('{} {} {} {} {} {}'.format(
-#           request.form['chrom'],
-#           request.form['position'],
-#           os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(in_fasta.filename)),
-#           os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(in_gff.filename)),
-#           os.path.join(app.config['UPLOAD_FOLDER'], ref_fasta_download ),
-#           os.path.join(app.config['UPLOAD_FOLDER'], ref_gff_download ) ))
-#
+      command =  'python3 reform.py --chrom {} --position {} --in_fasta {} --in_gff {} --ref_fasta {} --ref_gff {}'.format(
+           request.form['chrom'],
+           request.form['position'],
+           os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(in_fasta.filename)),
+           os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(in_gff.filename)),
+           ref_fasta_download,
+           ref_gff_download ) 
+      print( command )
+      os.system( command )
 
-      return 'files uploaded successfully'
+      return 'job submitted'
 
-		
 if __name__ == '__main__':
    app.run(debug = True)
