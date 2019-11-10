a07021251d866bbe0d7f0a1fbc66f0f911d9e631
Eric Borenstein <e.s.borenstein@gmail.com>
2019-11-09 23:31:40 -0500

    Removed test default values

--- forms.py
+++ forms.py
@@ -1,8 +1,6 @@
+from wtforms import Form, StringField, FileField
+from wtforms.validators import URL, Optional, InputRequired, Email
 from flask_wtf.file import FileRequired, FileAllowed
-from wtforms import FileField
-from wtforms import Form, StringField, validators
-from wtforms.validators import URL, Optional, InputRequired, Email, DataRequired
-from validators import RequiredIf, NotRequiredIf
 
 ALLOWED_EXTENSIONS = {'fa', 'gff', 'gff3', 'gtf', 'fasta', 'fna', 'tar', 'gz'}
 
@@ -12,21 +10,15 @@ class SubmitJob(Form):
                         description="When job is complete this e-mail will receive the download links",
                         render_kw={
                             "autofocus": "",
-                            "value": "eb167@nyu.edu"
                         },
                         validators=[
-                            # Optional()
                             InputRequired(),
                             Email()
                         ])
 
     chrom = StringField('Chromosome',
                         description="ID of the chromosome to modify",
-                        render_kw={
-                            "value": "X"
-                        },
                         validators=[
-                            # Optional()
                             InputRequired()
                         ])
 
@@ -34,30 +26,22 @@ class SubmitJob(Form):
     position = StringField('Position',
                            description="Position in chromosome at which to insert <in_fasta>. Can use -1 to add to end "
                                        "of chromosome. Note: Position is 0-based",
-                           render_kw={
-                               "value": "3"
-                           },
                            validators=[Optional()])
     # OR
     upstream_fasta = FileField('Upstream Sequence',
                                description="FASTA file with upstream sequence.",
-                               # validators=[Optional()])
                                validators=[
-                                   # NotRequiredIf("position", message="error")
                                    Optional()
                                ])
     downstream_fasta = FileField('Downstream Sequence',
                                  description="FASTA file with downstream sequence.",
-                                 # validators=[Optional()])
                                  validators=[
-                                     # NotRequiredIf("position", message="error")
                                      Optional()
                                  ])
 
     # Uploads
     in_fasta = FileField('Inserted Sequence (FASTA)',
                          description="New sequence to be inserted into reference genome.",
-                         # validators=[Optional()])
                          validators=[
                              Optional(),
                              FileAllowed([ALLOWED_EXTENSIONS], 'Invalid File Type'),
@@ -65,7 +49,6 @@ class SubmitJob(Form):
                          ])
     in_gff = FileField('Inserted Reference (gff3 or gtf)',
                        description="GFF file describing new FASTA sequence to be inserted.",
-                       # validators=[Optional()])
                        validators=[
                            Optional(),
                            FileAllowed([ALLOWED_EXTENSIONS], 'Invalid File Type'),
@@ -76,9 +59,7 @@ class SubmitJob(Form):
                             description="URL to reference FASTA file.",
                             render_kw={
                                 "placeholder": "Enter Reference URL",
-                                "value": "https://raw.githubusercontent.com/gencorefacility/reform/master/test_data/7/ref.fa"
                             },
-                            # validators=[Optional()])
                             validators=[
                                 URL(),
                                 InputRequired()
@@ -87,9 +68,7 @@ class SubmitJob(Form):
                           description="URL to reference gff file.",
                           render_kw={
                               "placeholder": "Enter Reference URL",
-                              "value": "https://raw.githubusercontent.com/gencorefacility/reform/master/test_data/7/ref.gtf"
                           },
-                          # validators=[Optional()])
                           validators=[
                               URL(),
                               InputRequired()
