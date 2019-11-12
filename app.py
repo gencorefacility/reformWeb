eb938089de451c4f80ccded1e98484f587848ea9
Eric Borenstein <e.s.borenstein@gmail.com>
2019-11-12 15:48:21 -0500

    redis

--- app.py
+++ app.py
@@ -1,20 +1,12 @@
 import datetime
-import os
-import subprocess
 
-import wget
-
-from flask import Flask, render_template, request, flash, url_for, send_file, Markup
-from werkzeug.utils import redirect
-from werkzeug.utils import secure_filename
+from flask import Flask, render_template, send_file
+from flask_mail import Mail
+from redis import Redis
+from rq import Queue
 
 from forms import SubmitJob
-import sqlite3
-
-from flask_mail import Mail, Message
-from rq import Queue
-from rq.job import Job
-from worker import conn
+from job import *
 
 app = Flask(__name__)
 # TODO: Move this secret_key out
@@ -27,8 +19,6 @@ app.config['MAIL_PASSWORD'] = ''
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = False
 mail = Mail(app)
-
-q = Queue(connection=conn)
 
 # TODO: Remove when going into production
 if os.name == 'nt':  # If Windows
@@ -111,34 +101,25 @@ def submit():
                 upload(target_dir, 'upstream_fasta')
                 upload(target_dir, 'downstream_fasta')
 
-            job = q.enqueue_call(
-                func=redis, args=(target_dir, timestamp, con), result_ttl=5000
-            )
-            print(job.get_id())
+            redis_conn = Redis()
+            q = Queue(connection=redis_conn)
 
-            # # (4) Download files from user provided URLs to server
-            # ref_fasta = download(target_dir, 'ref_fasta')
-            # ref_gff = download(target_dir, 'ref_gff')
-            #
-            # # (5) Run the reform.py
-            # try:
-            #     flash(u'Job ' + timestamp + ' submitted', 'info')
-            #     runReform(target_dir, ref_fasta, ref_gff, timestamp)
-            #
-            #     send_email(request.form['email'], timestamp)
-            #
-            #     flash(Markup('click <a href="./download/' + timestamp + '">here to download</a>'), 'info')
-            #
-            #     con.execute("UPDATE submissions SET status=? where timestamp=? ", ("complete", timestamp))
-            #     con.commit()
-            #     con.close()
-            #     return redirect(url_for('submit'))
-            #
-            # except:
-            #     flash(u'Job ' + timestamp + ' failed', 'error')
-            #     con.execute("UPDATE submissions SET status=? where timestamp=?", ("failed", timestamp))
-            #     con.commit()
-            #     con.close()
+            job = q.enqueue(redisjob, args=(target_dir,
+                                            timestamp,
+                                            request.form['email'],
+                                            request.form['chrom'],
+                                            request.files[
+                                                'upstream_fasta'].filename,
+                                            request.files[
+                                                'downstream_fasta'].filename,
+                                            request.form['position'],
+                                            request.form['ref_fasta'],
+                                            request.form['ref_gff'],
+                                            request.files[
+                                                'in_fasta'].filename,
+                                            request.files[
+                                                'in_gff'].filename))
+            print(job.get_id())
 
     return render_template('form.html', form=form)
 
@@ -152,114 +133,6 @@ def downloadFile(timestamp):
         flash(Markup('click <a href="./download/' + timestamp + '">here to download</a>'), 'info')
         flash("Download Error: File does not exist - " + path, 'error')
         return redirect(url_for('submit'))
-
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-def verify_uploads(file):
-    fileObj = request.files[file]
-
-    if fileObj.filename == '':
-        flash('No ' + file + ' file selected for uploading', 'error')
-        return False
-    if fileObj and allowed_file(fileObj.filename):
-        return True
-    else:
-        flash('Invalid File Type for ' + file, 'error')
-        return False
-
-
-def upload(target_dir, file):
-    fileObj = request.files[file]
-    # make the directory based on timestamp
-    os.system('mkdir ' + target_dir)
-    # save the file
-    fileObj.save(os.path.join(target_dir,
-                              secure_filename(fileObj.filename)))
-
-
-def download(target_dir, file):
-    URL = request.form[file]
-    if URL:
-        return wget.download(URL, target_dir)
-
-
-def runReform(target_dir, ref_fasta, ref_gff, timestamp):
-    os.system("mkdir results/" + timestamp)
-    if request.form['position']:
-        command = 'python reform.py --chrom {} --position {} --in_fasta {} --in_gff {} --ref_fasta {} --ref_gff {} ' \
-                  '--output_dir {}'.format(
-            request.form['chrom'],
-            request.form['position'],
-            os.path.join(target_dir, secure_filename(request.files['in_fasta'].filename)),
-            os.path.join(target_dir, secure_filename(request.files['in_gff'].filename)),
-            ref_fasta,
-            ref_gff,
-            "./results/" + timestamp + "/"
-        )
-    else:
-        command = 'python reform.py --chrom {} --upstream_fasta {} --downstream_fasta {} --in_fasta {} --in_gff {} ' \
-                  '--ref_fasta {} --ref_gff {} --output_dir {}'.format(
-            request.form['chrom'],
-            os.path.join(target_dir, secure_filename(request.files['upstream_fasta'].filename)),
-            os.path.join(target_dir, secure_filename(request.files['downstream_fasta'].filename)),
-            os.path.join(target_dir, secure_filename(request.files['in_fasta'].filename)),
-            os.path.join(target_dir, secure_filename(request.files['in_gff'].filename)),
-            ref_fasta,
-            ref_gff,
-            "./results/" + timestamp + "/"
-        )
-    flash(command, 'debug')
-
-    output = subprocess.getoutput(command)
-    print("OUTPUT IS " + output + "<-------")
-    # os.system(command)
-
-    os.system('tar -czf results/' + timestamp + '/' + timestamp + '.tar.gz -C results/' + timestamp + '/ .')
-
-
-def create_db():
-    conn = sqlite3.connect('database.db')
-    conn.execute(
-        'CREATE TABLE submissions (timestamp TEXT, email TEXT, status TEXT, chrom TEXT, upstream_fasta TEXT, '
-        'downstream_fasta TEXT, position TEXT, ref_fasta TEXT, ref_gff TEXT, in_fasta TEXT, in_gff TEXT)')
-    conn.close()
-
-
-def send_email(email, timestamp):
-    msg = Message('reform results', sender='reform-test@nyu.edu', recipients=[email])
-    print("TIMESTAMP=" + timestamp)
-    msg.html = "reform job complete. <a href='http://localhost:5000/download/" + timestamp + "'> Click here to download " \
-                                                                                             "results.</a> "
-    mail.send(msg)
-
-
-def redis(target_dir, timestamp, con):
-    # (4) Download files from user provided URLs to server
-    ref_fasta = download(target_dir, 'ref_fasta')
-    ref_gff = download(target_dir, 'ref_gff')
-
-    # (5) Run the reform.py
-    try:
-        flash(u'Job ' + timestamp + ' submitted', 'info')
-        runReform(target_dir, ref_fasta, ref_gff, timestamp)
-
-        send_email(request.form['email'], timestamp)
-
-        flash(Markup('click <a href="./download/' + timestamp + '">here to download</a>'), 'info')
-
-        con.execute("UPDATE submissions SET status=? where timestamp=? ", ("complete", timestamp))
-        con.commit()
-        con.close()
-        return redirect(url_for('submit'))
-
-    except:
-        flash(u'Job ' + timestamp + ' failed', 'error')
-        con.execute("UPDATE submissions SET status=? where timestamp=?", ("failed", timestamp))
-        con.commit()
-        con.close()
 
 
 if __name__ == '__main__':
---
+++ job.py
@@ -0,0 +1,132 @@
+import os
+import sqlite3
+import subprocess
+
+import wget
+from flask import request, flash, url_for, Markup
+from flask_mail import Message, Mail
+from werkzeug.utils import redirect
+from werkzeug.utils import secure_filename
+
+from app import ALLOWED_EXTENSIONS, app
+
+app.config['MAIL_SERVER'] = 'smtp.nyu.edu'
+app.config['MAIL_PORT'] = 25
+app.config['MAIL_USERNAME'] = 'reform-test@nyu.edu'
+app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = False
+mail = Mail(app)
+
+
+def redisjob(target_dir, timestamp, email, chrom, upstream_fasta, downstream_fasta, position, ref_fastaURL, ref_gffURL,
+             in_fasta, in_gff):
+    # (4) Download files from user provided URLs to server
+    ref_fasta = download(target_dir, ref_fastaURL)
+    ref_gff = download(target_dir, ref_gffURL)
+
+    # (5) Run the reform.py
+    try:
+        # flash(u'Job ' + timestamp + ' submitted', 'info')
+        runReform(target_dir, ref_fasta, ref_gff, timestamp, position, chrom, in_fasta, in_gff, upstream_fasta,
+                  downstream_fasta)
+
+        send_email(email, timestamp)
+
+        # flash(Markup('click <a href="./download/' + timestamp + '">here to download</a>'), 'info')
+
+        # con.execute("UPDATE submissions SET status=? where timestamp=? ", ("complete", timestamp))
+        # con.commit()
+        # con.close()
+        return redirect(url_for('submit'))
+
+    except:
+        print("except")
+        # flash(u'Job ' + timestamp + ' failed', 'error')
+        # con.execute("UPDATE submissions SET status=? where timestamp=?", ("failed", timestamp))
+        # con.commit()
+        # con.close()
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def verify_uploads(file):
+    fileObj = request.files[file]
+
+    if fileObj.filename == '':
+        flash('No ' + file + ' file selected for uploading', 'error')
+        return False
+    if fileObj and allowed_file(fileObj.filename):
+        return True
+    else:
+        flash('Invalid File Type for ' + file, 'error')
+        return False
+
+
+def upload(target_dir, file):
+    fileObj = request.files[file]
+    # make the directory based on timestamp
+    os.system('mkdir ' + target_dir)
+    # save the file
+    fileObj.save(os.path.join(target_dir,
+                              secure_filename(fileObj.filename)))
+
+
+def download(target_dir, URL):
+    if URL:
+        return wget.download(URL, target_dir)
+
+
+def runReform(target_dir, ref_fasta, ref_gff, timestamp, position, chrom, in_fasta, in_gff, upstream_fasta,
+              downstream_fasta):
+    os.system("mkdir results/" + timestamp)
+    if position:
+        command = 'python reform.py --chrom {} --position {} --in_fasta {} --in_gff {} --ref_fasta {} --ref_gff {} ' \
+                  '--output_dir {}'.format(chrom,
+                                           position,
+                                           os.path.join(target_dir, secure_filename(in_fasta)),
+                                           os.path.join(target_dir, secure_filename(in_gff)),
+                                           ref_fasta,
+                                           ref_gff,
+                                           "./results/" + timestamp + "/"
+                                           )
+    else:
+        command = 'python reform.py --chrom {} --upstream_fasta {} --downstream_fasta {} --in_fasta {} --in_gff {} ' \
+                  '--ref_fasta {} --ref_gff {} --output_dir {}'.format(chrom,
+                                                                       os.path.join(target_dir,
+                                                                                    secure_filename(upstream_fasta)),
+                                                                       os.path.join(target_dir,
+                                                                                    secure_filename(downstream_fasta)),
+                                                                       os.path.join(target_dir,
+                                                                                    secure_filename(in_fasta)),
+                                                                       os.path.join(target_dir,
+                                                                                    secure_filename(in_gff)),
+                                                                       ref_fasta,
+                                                                       ref_gff,
+                                                                       "./results/" + timestamp + "/"
+                                                                       )
+    flash(command, 'debug')
+
+    output = subprocess.getoutput(command)
+    print("OUTPUT IS " + output + "<-------")
+    # os.system(command)
+
+    os.system('tar -czf results/' + timestamp + '/' + timestamp + '.tar.gz -C results/' + timestamp + '/ .')
+
+
+def create_db():
+    conn = sqlite3.connect('database.db')
+    conn.execute(
+        'CREATE TABLE submissions (timestamp TEXT, email TEXT, status TEXT, chrom TEXT, upstream_fasta TEXT, '
+        'downstream_fasta TEXT, position TEXT, ref_fasta TEXT, ref_gff TEXT, in_fasta TEXT, in_gff TEXT)')
+    conn.close()
+
+
+def send_email(email, timestamp):
+    msg = Message('reform results', sender='reform-test@nyu.edu', recipients=[email])
+    print("TIMESTAMP=" + timestamp)
+    msg.html = "reform job complete. <a href='http://localhost:5000/download/" + timestamp + "'> Click here to download " \
+                                                                                             "results.</a> "
+    mail.send(msg)
