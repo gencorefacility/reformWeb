0614484c4b6e697014f53a50f520eb5b40dba677
Eric Borenstein <e.s.borenstein@gmail.com>
2019-11-19 15:06:01 -0500

    bugfix and output

--- run.sh
+++ run.sh
@@ -17,36 +17,37 @@ elif [ "$#" -eq 10 ]; then
 fi
 
 # Download files from user provided URLs to server
-wget $ref_fastaURL -O $target_dir
+echo "wget ref_fasta"
 ref_fasta=$(basename "$ref_fastaURL")
-wget ref_gffURL -O $target_dir
-ref_gff=$(basename "$ref_gffURL")
+wget -nv $ref_fastaURL -O $target_dir/$ref_fasta
 
-echo "ref_fasta=$ref_fasta & ref_gff=$ref_gff"
+echo "wget ref_gff"
+ref_gff=$(basename "$ref_gffURL")
+wget -nv $ref_gffURL -O $target_dir/$ref_gff
 
 # Are the downloads compressed (gzip)
 if [[ ${ref_fasta: -3} == ".gz" ]]; then
+  echo "gunzip $target_dir/$ref_fasta"
   gunzip $target_dir/$ref_fasta
   ref_fasta=${ref_fasta:: -3}
 fi
 
 if [[ ${ref_gff: -3} == ".gz" ]]; then
+  echo "gunzip $target_dir/$ref_gff"
   gunzip $target_dir/$ref_gff
   ref_gff=${ref_gff:: -3}
 fi
-
-echo "ref_fasta=$ref_fasta & ref_gff=$ref_gff"
 
 # Run reform.py
 mkdir -p ./results/$timestamp
 
 if [ ! -z "$position" ]; then
-  echo '/home/reform/venv/bin/python reform.py --chrom $chrom --position $position --in_fasta $in_fasta \
-  --in_gff $in_gff --ref_fasta $ref_fasta --ref_gff $ref_gff --output_dir "./results/$timestamp/"'
+  /home/reform/venv/bin/python reform.py --chrom $chrom --position $position --in_fasta $in_fasta \
+  --in_gff $in_gff --ref_fasta $ref_fasta --ref_gff $ref_gff --output_dir "./results/$timestamp/"
 else
-  echo '/home/reform/venv/bin/python reform.py --chrom $chrom --upstream_fasta $upstream_fasta \
+  /home/reform/venv/bin/python reform.py --chrom $chrom --upstream_fasta $upstream_fasta \
   --downstream_fasta $downstream_fasta --in_fasta $in_fasta --in_gff $in_gff --ref_fasta $ref_fasta \
-  --ref_gff $ref_gff --output_dir "./results/$timestamp/"'
+  --ref_gff $ref_gff --output_dir "./results/$timestamp/"
 fi
 
 #tar -czf ./results/$timestamp/$timestamp.tar.gz -C results/$timestamp / .
